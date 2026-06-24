97bc1bbb4f1e3b14629f6e2afa7e7ceedf9c67dd
Ozy2410 <ramadhanahmadfauzi3@gmail.com>
2026-06-24 16:46:31 +0700

    Fix Colab Disk Space Crash by converting BMP to JPG on the fly

--- Code/generate_dataset.py
+++ Code/generate_dataset.py
@@ -22,31 +22,31 @@ for cls in CLASSES:
     # Full slide images
     class_dir = os.path.join(SIPAKMED_ROOT, cls, cls)
     if os.path.exists(class_dir):
-        for bmp in sorted(glob.glob(os.path.join(class_dir, '*.bmp'))):
-            txt = os.path.splitext(bmp)[0] + '.txt'
+        for img_file in sorted(glob.glob(os.path.join(class_dir, '*.[bj][mp][pg]*'))):
+            txt = os.path.splitext(img_file)[0] + '.txt'
             if os.path.exists(txt):
-                lines.append(os.path.relpath(bmp, '.').replace('\\', '/') + '\n')
+                lines.append(os.path.relpath(img_file, '.').replace('\\', '/') + '\n')
             else:
                 missing_labels += 1
 
     # Cropped images
     cropped_dir = os.path.join(SIPAKMED_ROOT, cls, cls, 'CROPPED')
     if os.path.exists(cropped_dir):
-        for bmp in sorted(glob.glob(os.path.join(cropped_dir, '*.bmp'))):
-            txt = os.path.splitext(bmp)[0] + '.txt'
+        for img_file in sorted(glob.glob(os.path.join(cropped_dir, '*.[bj][mp][pg]*'))):
+            txt = os.path.splitext(img_file)[0] + '.txt'
             if os.path.exists(txt):
-                lines.append(os.path.relpath(bmp, '.').replace('\\', '/') + '\n')
+                lines.append(os.path.relpath(img_file, '.').replace('\\', '/') + '\n')
             else:
                 missing_labels += 1
 
 # 2. Add Augmented Images (if any exist)
 if os.path.exists(AUG_ROOT):
-    # Augmented images were saved as .bmp, not .jpg!
-    for bmp in sorted(glob.glob(os.path.join(AUG_ROOT, '*.bmp'))):
-        basename = os.path.basename(bmp)
+    # Augmented images were saved as .bmp or .jpg!
+    for img_file in sorted(glob.glob(os.path.join(AUG_ROOT, '*.[bj][mp][pg]*'))):
+        basename = os.path.basename(img_file)
         txt = os.path.join(SIPAKMED_ROOT, 'aug_labels', os.path.splitext(basename)[0] + '.txt')
         if os.path.exists(txt):
-            lines.append(os.path.relpath(bmp, '.').replace('\\', '/') + '\n')
+            lines.append(os.path.relpath(img_file, '.').replace('\\', '/') + '\n')
         else:
             missing_labels += 1
 
